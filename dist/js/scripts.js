9bf7c0c5d2185e8e23e119ac143494428735797a
Abrar051 <shahriar1410051@gmail.com>
2022-04-15 23:33:48 -0700

    page editing

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
+* Start Bootstrap - Opportuniteens v7.0.11 (undefined)
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
 */
